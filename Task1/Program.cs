6892e981b9ae13fabfd33c67adcf0dce48b91ff9
Lukianova <polina-03-05@yandex.ru>
2022-12-09 12:51:46 +0300

    написали программу для вычесления высокосный год или нет

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите год:");
+
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 400 == 0 || n % 100 != 0 && n % 4 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
