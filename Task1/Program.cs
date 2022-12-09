5e36424ec6e8085c9c8cab0ebe9fb7af4a01a0a9
Lukianova <polina-03-05@yandex.ru>
2022-12-09 13:59:51 +0300

    Выполнили задачу 3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,6 +3,7 @@
 Console.Write("Введите год:");
 
 int n = Convert.ToInt32(Console.ReadLine());
+
 if (n % 400 == 0 || n % 100 != 0 && n % 4 == 0)
     Console.WriteLine("yes");
 else
---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число1:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число2:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+    
