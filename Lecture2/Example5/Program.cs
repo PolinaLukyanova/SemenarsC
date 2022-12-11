a1b1935ec72160aa0ca6e7c217a77a2bb761c1c9
Lukianova <polina-03-05@yandex.ru>
2022-12-11 23:13:35 +0300

    поиск позиции нужного эллемента с помощью методов

---
+++ Lecture2/Example5/Program.cs
@@ -0,0 +1,51 @@
+﻿// поиск позиции нужного эллемента с помощью метода
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray( int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+ 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10]; // 1. определили массив из 10 эллементов
+
+FillArray(array); // 2. вызвали метод FillArray который заполнил наш массив
+PrintArray(array); //3. метот PrintArray вывел наш массив на экран
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 555);
+Console.WriteLine(pos);
