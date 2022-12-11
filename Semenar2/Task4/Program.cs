7c298183243c13bf72b40ac67df2c24b35420720
Lukianova <polina-03-05@yandex.ru>
2022-12-12 01:09:50 +0300

    выполнили задачу 14 на двойное условие

---
+++ Semenar2/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача 14. Напишите программу , которая принимает 
+// на ввод число и проверяет , кратно ли оно 
+// одновременно 7 и 23
+
+Console.Clear();
+
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ( n % 7 == 0 )
+{
+    if (n % 23 == 0)
+    Console.WriteLine($"Число {n} кратно 7 и 23");
+    else 
+    Console.WriteLine($"Число {n} не кратно 7 и 23");
+}
+
+else 
+    Console.WriteLine($"Число {n} не кратно 7 и 23");
