fa26948b6acc4db2ad6738452e36c62b0a1e25b6
Lukianova <polina-03-05@yandex.ru>
2022-12-12 01:19:15 +0300

    выполнили задачу 16

---
+++ Semenar2/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 16. Напишите программу, которая принимает
+// на вход два числа и проверяет, является ли
+// одно число квадратом другого.
+
+Console.Clear();
+
+Console.Write("Введите число1:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число2:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n * n == m)
+    Console.WriteLine("yes");
+else if (m * m == n)
+    Console.WriteLine("yes");
+else 
+    Console.WriteLine("no");
