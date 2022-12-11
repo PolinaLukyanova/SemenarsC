8120e6f2b9094863853237db9a291cc2a0c41ba5
Lukianova <polina-03-05@yandex.ru>
2022-12-11 22:36:51 +0300

    написали программу поиска позиции нужного эллемена

---
+++ Lecture2/Example3/Program.cs
@@ -0,0 +1,21 @@
+﻿// поиск позиции нужного эллемента
+int[] array = { 1, 2, 8, 4, 5, 6, 7, 8 };
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;   //прерви
+    }
+    // index = index +1;
+    index++;
+}
+
+
+// можно сделать лучше
