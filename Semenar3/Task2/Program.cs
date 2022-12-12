b821482d017e7279fd5b180354736736125a2824
Lukianova <polina-03-05@yandex.ru>
2022-12-12 22:34:37 +0300

    Задача 18

---
+++ Semenar3/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 18. Напишите программу, которая по заданному 
+// номеру четверти, показывает диапазон возможных координат
+// точек в этой четверти(x и y).
+
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+double n =Convert.ToDouble(Console.ReadLine());
+
+
+
+if (n == 1 || n == 2)
+    Console.WriteLine("y > 0");
+else
+    Console.WriteLine("y < 0");
+if (n == 1 || n == 4)
+    Console.WriteLine("x > 0");
+else 
+    Console.WriteLine("x < 0");
+
