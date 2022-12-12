3e6db6f0b6c52d35849b4476feff8965291907e5
Lukianova <polina-03-05@yandex.ru>
2022-12-12 11:14:29 +0300

    выполнила задачу13

---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 100 && n < 1000)
+{
+    int n1 = n % 100;
+    int n2 = n / 10;
+    int n3 = n % 10;
+    Console.WriteLine($"{n3}");
+}
+
+else 
+Console.WriteLine("Третьей цифры нет");
