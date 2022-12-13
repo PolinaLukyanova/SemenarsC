a5ba7ddead7c58331c1593ac672d0bad23e76e6c
Lukianova <polina-03-05@yandex.ru>
2022-12-13 21:39:42 +0300

    save

---
+++ Lecture3/Exsampl1/Program.cs
@@ -0,0 +1,45 @@
+﻿// В тексте нужно все пробелы заменить черточками, маленькие 
+// буквы "к" заменить большими "К", а большие "С" заменить маленькими "с"
+
+string text = " – Я думаю, – сказал князь, улыбаясь, – что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде," 
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "fuhfh"
+// .           012
+// s[3] // f          вызов символа по индексу
+
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length; //получить длину строки, кол-во символов
+    for (int i = 0; i < length; i++)  // используя цикл for проходим 
+    // от 0 символа до конца строки делая слудующее действие
+    {
+        // если текущий символ совпал с тем символом который 
+        // мы хотим заменить, то в результат должны положить  
+        // новое значиние в виде строки $"{newValue}"
+
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        // если же совпадений нет то в result надо добавить текущий символ
+        else result = result + $"{text[i]}";
+    }
+    return result; //вернуть результат
+
+}
+// запустим программу, присвоим новые значения
+string newText = Replace(text, ' ', '|'); 
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К'); 
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С'); 
+Console.WriteLine(newText);
+
+
---
+++ Lecture3/Exsampl2/Program.cs
@@ -0,0 +1,42 @@
+﻿// сортировка эллементов в массиве
+
+//вводим массив
+int[] arr = {1, 4, 6, 7, 8, 6, 5 };
+
+//метод вывода массива на экран
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)  //проходим по массиву
+    {
+        Console.Write($"{array[i]} ");      //выводим на экран
+    }
+    Console.WriteLine();   //пустая страчка для удобства чтения
+}
+
+//метот который будет сортировать массив
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)  //проходим по всем эллементам массива
+    {
+        int minPosition = i;
+
+        //ищем минимальный эллемент
+        for (int j = i +1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
