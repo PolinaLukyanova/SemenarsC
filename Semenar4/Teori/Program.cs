313b3c90588b1a09b20bc8668d3f04694af4d72a
Lukianova <polina-03-05@yandex.ru>
2022-12-16 15:06:13 +0300

    шаблон для работы с массивом

---
+++ Semenar4/Teori/Program.cs
@@ -0,0 +1,12 @@
+﻿// array , Function
+
+// Базовый шаблон для работы с массивами
+
+Console.Clear();
+Console.Write("Введите колличество элементов в массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11); //[1, 10]
+
+Console.WriteLine($"[{string.Join(", ", array)}]"); //функция string делает строку из массива. функция join указываем через что надо разделять эллементы массива
