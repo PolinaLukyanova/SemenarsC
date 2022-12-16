ef520768dd0123b3a4f93303612a6e093a18a82f
Lukianova <polina-03-05@yandex.ru>
2022-12-16 15:17:57 +0300

    шаблон генирации дробных сичел в массиве

---
+++ Semenar4/DoubleNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// Array
+// Double numbers - дробные числа
+
+Console.Clear();
+Console.Write("Введите колличество элементов в массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for (int i = 0; i < array.Length; i++)
+{
+    //         округляет до заданного значения |    * (end - begin) + begin
+    array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2); 
+}
+Console.WriteLine($"[{string.Join(", ", array)}]"); //функция string делает строку из массива. функция join указываем через что надо разделять эллементы массива
