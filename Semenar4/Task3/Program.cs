98ee763d18f2e7ea7a21de1cd5456d62f4f9f9a2
Lukianova <polina-03-05@yandex.ru>
2022-12-16 17:21:51 +0300

    задача 28

---
+++ Semenar4/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*  Задача 28. Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), factorial = 1;
+for (int i = 2; n >= i; ++i)
+{
+    factorial = factorial * i;
+}
+Console.WriteLine(factorial);
