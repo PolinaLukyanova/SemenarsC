0a4235d7316fdeeaee9118115b911898e21cd1f4
Lukianova <polina-03-05@yandex.ru>
2022-12-16 17:30:21 +0300

    Задача 30

---
+++ Semenar4/Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Задача 30.  Напишите программу, которая выводит 
+массив из 8 элементов, заполненый нулями и еденицами
+в случайном порядке. */
+
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 2); //[0, 1]
+Console.WriteLine($"[{string.Join(", ", array)}]");
