a56d7cef682cc8c0c635b92b806ba8a5012b32fb
Lukianova <polina-03-05@yandex.ru>
2022-12-16 14:09:10 +0300

    зешение дополнительное задачи нахождение второго максимум

---
+++ Homework2/Task+/Program.cs
@@ -0,0 +1,19 @@
+﻿// найти второй максимум 
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), max1 = n, max2 = -1;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (max1 < n)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if (max2 < n)
+    {
+        max2 = n;
+    }
+}
+Console.WriteLine(max2);
