48bf5cbdf97a5205e31f5f525ecc4d22577c9fde
Lukianova <polina-03-05@yandex.ru>
2022-12-23 10:18:59 +0300

    метод для заполнения матрици случайными числами

---
+++ Lecture4/Exsample2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Заполнение матрицы случайными числами
+
+void PrintArray(int[,] matr)
+{
+    for ( int i = 0; i < matr.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+// метод для заполнения матрици случайными числами
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);//[1; 10)
+        }
+    }
+}
+Console.Clear();
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+
+
+
+
