69ff20e5fb9246cf08b1c7ab2c86d1972ebdbc43
Lukianova <polina-03-05@yandex.ru>
2022-12-27 15:03:16 +0300

    Рекурсия.Вычесление факторила

---
+++ Lecture4/Exsample4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+double Factorial(int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+Console.Clear();
+for (int i = 1; i < 40; i++)
+{
+ Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+Console.WriteLine(Factorial(40));
