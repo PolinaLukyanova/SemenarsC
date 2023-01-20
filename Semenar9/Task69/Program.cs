a3eb0e4aca8eb96200bddb0001d1b866081f7804
Lukianova <polina-03-05@yandex.ru>
2023-01-20 19:47:15 +0300

    Задача 69

---
+++ Semenar9/Task69/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 69. Напишите программу,
+ которая на вход принимает два числа A и B, 
+ и возводит число А в целую степень B с помощью 
+ рекурсии. */
+int rec(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return rec(a, b - 1) * a ;
+}
+
+
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(a, b));
