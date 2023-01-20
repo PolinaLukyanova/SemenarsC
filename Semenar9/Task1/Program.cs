f4086f66e21dfa8724d6a1cdec744887401f74f1
Lukianova <polina-03-05@yandex.ru>
2023-01-20 17:21:32 +0300

    сумма чисел с помощью рекурсии

---
+++ Semenar9/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// нахождение суммы чисел с помощью рекурсии
+
+int sum(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return sum(a + 1, b - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(n, m));
